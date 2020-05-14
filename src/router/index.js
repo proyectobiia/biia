3a2240e5111e383daf6fda66740825ae399088f9
Edgardo Ramírez <soldiercrp@gmail.com>
2020-05-14 06:08:10 -0500

    ADD: Scroll behaviour for vue router

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,13 @@ Vue.use(VueRouter)
 
 export default new VueRouter({
   mode: 'history',
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  },
   routes: [
     {
       path: '/',
