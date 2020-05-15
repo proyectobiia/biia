e0f3bf7e83917588e668952df8aee3e1da713578
Edgardo Ramírez <soldiercrp@gmail.com>
2020-05-15 07:12:42 -0500

    UPDATE: Navbar home  background

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -233,7 +233,7 @@
     &.fixed {
       position: fixed;
       top: 0;
-      background: $turquoise;
+      background: $dark;
       z-index: 3;
       box-shadow: 0 10px 20px 0 rgba(0, 0, 0, 0.16);
 
