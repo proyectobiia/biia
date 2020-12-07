e7979b872428ca3e6ceeab9eee3a80bc1eaa5832
Jackson <erickgj14@hotmail.com>
2020-12-06 21:41:16 -0600

    BIIA

--- src/app/components/academias/academias.component.ts
+++ src/app/components/academias/academias.component.ts
@@ -11,7 +11,7 @@ export class AcademiasComponent implements OnInit {
   academiaList
   showAcademiaInfo = false
   academiaName
-  academiaDescription
+  academiaDescription;
   academiaPath
   academiaDiscount
   planList
